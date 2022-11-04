df01f1152a18058f319286ff8b5d43b9ac81a2dc
maishu <ansiboy@163.com>
2022-11-04 17:20:50 +0800

    把 build 的设为开发模式

--- front/scripts/build.js
+++ front/scripts/build.js
@@ -1,8 +1,12 @@
 'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
+// process.env.BABEL_ENV = 'production';
 process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+//========================================
+/** 说明：设为生成模式, 生成的 js 报错 */
+process.env.NODE_ENV = 'development';
+//========================================
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -74,13 +78,13 @@ checkBrowsers(paths.appPath, isInteractive)
         console.log(warnings.join('\n\n'));
         console.log(
           '\nSearch for the ' +
-            chalk.underline(chalk.yellow('keywords')) +
-            ' to learn more about each warning.'
+          chalk.underline(chalk.yellow('keywords')) +
+          ' to learn more about each warning.'
         );
         console.log(
           'To ignore, add ' +
-            chalk.cyan('// eslint-disable-next-line') +
-            ' to the line before.\n'
+          chalk.cyan('// eslint-disable-next-line') +
+          ' to the line before.\n'
         );
       } else {
         console.log(chalk.green('Compiled successfully.\n'));
@@ -184,7 +188,7 @@ function build(previousFileSizes) {
           console.log(
             chalk.yellow(
               '\nTreating warnings as errors because process.env.CI = true.\n' +
-                'Most CI servers set it automatically.\n'
+              'Most CI servers set it automatically.\n'
             )
           );
           return reject(new Error(filteredWarnings.join('\n\n')));
