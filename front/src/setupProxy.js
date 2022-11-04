46aa3ae99d46b7d68867a410ebf53b946484c8da
maishu <ansiboy@163.com>
2022-11-04 17:35:42 +0800

    修改代理

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -1,19 +1,11 @@
 "use strict";
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
-    app.use(createProxyMiddleware("/user", {
+    app.use(createProxyMiddleware("/api", {
         target: 'http://127.0.0.1:42986', // 请求的地址
         // changeOrigin: true,
         // pathRewrite: {
         //   "^/user": "/user"
         // }
-    }));
-    app.use(createProxyMiddleware("/media", {
-        target: 'http://127.0.0.1:42986', // 请求的地址
-        // changeOrigin: true,
-    }));
-    app.use(createProxyMiddleware("/medias", {
-        target: 'http://127.0.0.1:42986', // 请求的地址
-        // changeOrigin: true,
     }));
 };
