120d5a1d10fc5b1958907ede08174be27deb61f2
Shu Mai <ansiboy@163.com>
2023-02-01 10:30:52 +0800

    userData file 自动创建

--- src/web-server/user-data.ts
+++ src/web-server/user-data.ts
@@ -1,19 +1,30 @@
 import * as fs from "fs";
 import * as path from "path";
-import errors from "./controllers/errors";
 
 export interface UserDataJSON {
   "user": {
     "name": string,
     "password": string
   },
-  "token": string,
-  "remoteControl": boolean
+  token?: string,
+  remoteControl?: boolean
 }
 
+const DefaultUserName = "admin";
+const DefaultPassword = "000000";
 
 let userDataFilePath = path.join(__dirname, "../../user-data.json");
-if (!fs.existsSync(userDataFilePath)) throw errors.fileNotExists(userDataFilePath)
+if (!fs.existsSync(userDataFilePath)) {
+  let d: UserDataJSON = {
+    user: {
+      name: DefaultUserName,
+      password: DefaultPassword,
+    }
+  }
+  let content = Buffer.from(JSON.stringify(d));
+  fs.writeFileSync(userDataFilePath, content, { flag: "a" });
+}
+//throw errors.fileNotExists(userDataFilePath)
 
 export default class UserData {
 
